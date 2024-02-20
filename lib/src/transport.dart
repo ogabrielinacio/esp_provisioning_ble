3f3e1730969cc0234b0ed6305e4e9b4ded0fe7e5
Iván Hernández Cazorla <ivan@corujadigital.tech>
2024-02-20 10:28:39 +0000

    write docs for ProvTransport

--- lib/src/transport.dart
+++ lib/src/transport.dart
@@ -1,5 +1,7 @@
 import 'dart:typed_data';
 
+/// A set of necessary methods to implement with a Bluetooth package
+/// and provide the provisioning transport functionality to [EspProv].
 abstract class ProvTransport {
   Future<bool> connect();
 
